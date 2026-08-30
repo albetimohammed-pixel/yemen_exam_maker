d64bdb71f4b24d1cada3892a8d0b0e84d9cea391
albetimohammed-pixel <albetimohammed@gmail.com>
2026-08-30 15:13:21 +0300

    Update app_colors.dart

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+class AppColors {
+  static const Color primaryPurple = Color(0xFF673AB7);
+  static const Color darkPurple = Color(0xFF4A148C);
+  static const Color backgroundDark = Color(0xFF1E1E2C);
+  static const Color cardDark = Color(0xFF2D2D3F);
+  static const Color accentGreen = Color(0xFF4CAF50);
+  static const Color accentGold = Color(0xFFFFC107);
+  static const Color canvasBg = Color(0xFFE0E0E0);
+  static const Color textLight = Color(0xFFF5F5F5);
+}
