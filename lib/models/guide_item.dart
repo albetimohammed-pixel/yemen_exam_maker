90fd51dab1f9d3c1de027749999db4aaeb74c7d9
albetimohammed-pixel <albetimohammed@gmail.com>
2026-08-30 15:20:44 +0300

    Create GuideItem model with title, description, and icon

---
+++ lib/models/guide_item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GuideItem {
+  final String title;
+  final String description;
+  final IconData icon;
+
+  GuideItem({
+    required this.title,
+    required this.description,
+    required this.icon,
+  });
+}
+
