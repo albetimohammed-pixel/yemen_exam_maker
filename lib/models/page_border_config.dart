ee970bd8c1712cbcb471a97012f5e239773c0115
albetimohammed-pixel <albetimohammed@gmail.com>
2026-08-30 14:55:29 +0300

    Add PageBorderConfig class with border properties

--- lib/models/page_border_config.dart
+++ lib/models/page_border_config.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
 
+class PageBorderConfig {
+  bool isEnabled;
+  Color outerBorderColor;
+  double outerBorderWidth;
+  double spacing;
+  Color innerBorderColor;
+  double innerBorderWidth;
+  double cornerRadius;
+  Color backgroundColor;
+
+  PageBorderConfig({
+    this.isEnabled = true,
+    this.outerBorderColor = Colors.green,
+    this.outerBorderWidth = 3.0,
+    this.spacing = 4.0,
+    this.innerBorderColor = Colors.black,
+    this.innerBorderWidth = 1.0,
+    this.cornerRadius = 0.0,
+    this.backgroundColor = Colors.white,
+  });
+}
