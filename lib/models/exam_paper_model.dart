f1445f1361ee813990becf899b61f6a9b5b5ad3b
albetimohammed-pixel <albetimohammed@gmail.com>
2026-08-31 21:23:29 +0300

    Update exam_paper_model.dart

--- lib/models/exam_paper_model.dart
+++ lib/models/exam_paper_model.dart
@@ -1,71 +1,59 @@
 import 'package:flutter/material.dart';
 
-// 1. نموذج السؤال
+enum HeaderStyle { ovalTriple, modernMinimal, republicEmblem }
+
+class ExamHeaderData {
+  String republic;
+  String ministry;
+  String directorate;
+  String schoolName;
+  String subject;
+  String grade;
+  String examType;
+  String section;
+  String date;
+  String duration;
+  String academicYear;
+  HeaderStyle style;
+  bool showEmblem;
+
+  ExamHeaderData({
+    this.republic = 'الجمهورية اليمنية',
+    this.ministry = 'وزارة التربية والتعليم',
+    this.directorate = 'مكتب التربية والتعليم',
+    this.schoolName = 'مدرسة النهضة الحديثة',
+    this.subject = 'اللغة العربية',
+    this.grade = 'الأول الثانوي',
+    this.examType = 'اختبار الشهر الثاني',
+    this.section = 'أ',
+    this.date = '2026 / 5 / 10 م',
+    this.duration = 'ساعتان',
+    this.academicYear = '2025 - 2026 م',
+    this.style = HeaderStyle.republicEmblem,
+    this.showEmblem = true,
+  });
+}
+
 class QuestionItem {
   String id;
-  String number; // رقم أو رمز السؤال (مثل: س1 أو أ)
-  String title; // نص السؤال الرئيسي
-  List<String> subItems; // الفقرات/النقاط الفرعية
-  int grade; // الدرجة
+  String number;
+  String title;
+  List<String> subItems;
+  double grade;
+  double fontSize;
   bool isBold;
   bool isItalic;
   bool isUnderline;
-  double fontSize;
 
   QuestionItem({
     required this.id,
     required this.number,
     required this.title,
-    this.subItems = const [],
-    this.grade = 0,
+    required this.subItems,
+    this.grade = 1.0,
+    this.fontSize = 11.0,
     this.isBold = false,
     this.isItalic = false,
     this.isUnderline = false,
-    this.fontSize = 12.0,
   });
 }
-
-// 2. نموذج الكليشة اليمنية الرسمية (ثانوية المكلا النموذجية)
-class YemeniHeaderModel {
-  String subject;
-  String day;
-  String date;
-  String examTitle;
-  String gradeLevel;
-  String academicYear;
-  String schoolName;
-  String directorate;
-  String duration;
-  String teacherName;
-
-  YemeniHeaderModel({
-    this.subject = 'المجتمع',
-    this.day = 'الاثنين',
-    this.date = '2026/04/20م',
-    this.examTitle = 'اختبار الشهري الثاني الفصل الدراسي الثاني',
-    this.gradeLevel = 'للصف الأول الثانوي',
-    this.academicYear = 'للعام 2025-2026م',
-    this.schoolName = 'ثانوية المكلا النموذجية للبنين',
-    this.directorate = 'مديرية المكلا',
-    this.duration = 'حصة',
-    this.teacherName = 'أ. أحمد علي',
-  });
-}
-
-// 3. نموذج حفظ الاختبارات الشامل
-class SavedExamModel {
-  String id;
-  String title;
-  String date;
-  YemeniHeaderModel header;
-  List<QuestionItem> questions;
-
-  SavedExamModel({
-    required this.id,
-    required this.title,
-    required this.date,
-    required this.header,
-    required this.questions,
-  });
-}
-
